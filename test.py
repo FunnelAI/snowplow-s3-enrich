8854314cac14aacd010aa06ebc2eb8d7f4fc22a9
Bulletninja <luis.linuxero@gmail.com>
2019-09-10 14:50:14 -0600

    Cleanup

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 """ Enricher test
 """
+import os
 import boto3
 from enricher import enrich
-import os
 
-S3 = boto3.client(
-    's3',
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("REGION_NAME"))
+S3 = boto3.client('s3',
+                  aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                  aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                  region_name=os.getenv("REGION_NAME"))
 
 enrich('snowplow-funnel-logs',
        'E2TOTHS7895X2G.2019-09-06-01.1c4dc51d.gz',
