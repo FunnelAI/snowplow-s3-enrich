db84ce3cc51d48f4340c82fbd2b9b4ad3a334a29
Bulletninja <luis.linuxero@gmail.com>
2019-09-10 16:39:22 -0600

    TODO: separate raw from enriched in S3

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@
 from enricher import enrich
 
 def lambda_handler(event, context):
+    #TODO: Move root file to RAW folder
+    #and processed file to ENRICHED folder
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
     enrich(bucket_name, file_key)
