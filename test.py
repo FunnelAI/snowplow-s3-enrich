1f0ba916f763ee4a402ced9418ccae8b1dc841e0
Bulletninja <luis.linuxero@gmail.com>
2019-09-10 14:48:49 -0600

    Add file to test locally (or otherwise) with env vars

---
+++ test.py
@@ -0,0 +1,15 @@
+""" Enricher test
+"""
+import boto3
+from enricher import enrich
+import os
+
+S3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("REGION_NAME"))
+
+enrich('snowplow-funnel-logs',
+       'E2TOTHS7895X2G.2019-09-06-01.1c4dc51d.gz',
+       S3=S3)
